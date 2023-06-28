961eaa20206b20bbcbd445968a9292852b23bc28
JotaPrimo <gestald118@gmail.com>
2023-06-28 17:47:50 -0400

    105. Link ativo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import "./Navbar.css";
 
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return <nav>
-    <Link to="/">Home</Link>
-    <Link to="about" >About</Link>
+    {/* <Link to="/">Home</Link>
+    <Link to="about" >About</Link> */}
+
+{/* <NavLink to="/" className={({isActive}) => (isActive ? '' : '' )}> Home </NavLink> */}
+
+    <NavLink to="/"> Home </NavLink>
+    <NavLink to="/about">Sobre</NavLink>
   </nav>;
 };
 
